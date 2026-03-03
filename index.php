6273d999bc698a0941c3cea55a5d4293a2a33625
Adeleye Ademiluyi <adeleye.ademiluyi.timilehin@gmail.com>
2026-03-03 07:29:14 -0500

    Add initial HTML structure for index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vulnerable Web Application</title>
+</head>
+<body>
+    <h1>Welcome to the Vulnerable Web Application</h1>
+    <p><a href="register.php">Register</a> | <a href="login.php">Login</a> | <a href="search.php">Search Products</a></p>
+</body>
+</html>
